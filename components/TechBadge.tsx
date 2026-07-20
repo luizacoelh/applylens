16fcb8aa8de8db6d6620cb640a21c502585d2b9d
Luiza Coelho <luizacoelho165@gmail.com>
2026-07-19 22:27:14 -0300

    fix: remove duplicated components causing build errors

--- components/TechBadge.tsx
+++
@@ -1,7 +0,0 @@
-export default function TechBadge({ tech }: { tech: string }) {
-  return (
-    <span className="rounded-full border border-[#378ADD]/40 bg-[#378ADD]/10 px-3 py-1 font-mono text-xs text-[#378ADD]">
-      {tech}
-    </span>
-  );
-}
